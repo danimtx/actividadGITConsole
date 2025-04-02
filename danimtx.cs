860c23ee338d21fedafba0b159da96523924bf82
danimtx <72873452daniel@gmail.com>
2025-04-01 21:53:24 -0400

    commit de danimtx

---
+++ danimtx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividadGitConsole
+{
+    public class danimtx
+    {
+        public danimtx()
+        {
+            Console.WriteLine("hola mundo, de danimtx");
+        }
+    }
+}
