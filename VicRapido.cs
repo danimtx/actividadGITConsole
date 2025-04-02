307a65c893367ea2f7816c14bf56e15e49edd750
victor30700 <gsaldana709@gmail.com>
2025-04-01 21:56:15 -0400

    commit de victorRapido

---
+++ VicRapido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividadGitConsole
+{
+    internal class VicRapido
+    {
+        public VicRapido()
+        {
+            Console.WriteLine("Victor saldana");
+        }
+    }
+}
